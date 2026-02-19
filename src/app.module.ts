15f016cfbd8b265ab29f84f81c0bfb4cd8285d19
Ferlina Estubo <estuboferlina@gmail.com>
2026-02-19 23:04:27 +0800

    add prisma service and module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WalletModule } from './wallet/wallet.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [WalletModule],
+  imports: [WalletModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
