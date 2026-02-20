00835357d2ec30e34f85fe9a17609641b8439e8a
Ferlina Estubo <estuboferlina@gmail.com>
2026-02-20 14:33:29 +0800

    fix app.module imports and wire WalletModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WalletModule } from './wallet/wallet.module';
-import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [WalletModule, PrismaModule],
+  imports: [WalletModule],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
